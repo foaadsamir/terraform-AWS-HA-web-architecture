1558e82f0396a92c4f98810bec5d2dc35510171b
fouad samir <foaadsamir1@gmail.com>
2026-01-15 02:15:10 +0200

    Add security groups for ALB and web servers

---
+++ security_groups.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+}
